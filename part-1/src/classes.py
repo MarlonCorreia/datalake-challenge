84ced448410034f047d83972eb1a15e75be5a05e
marloncorreia <malilocsimas26@gmail.com>
2020-10-14 20:35:08 -0300

    get hash from json body

--- part-1/src/classes.py
+++ part-1/src/classes.py
@@ -1,11 +1,30 @@
 from pydantic import BaseModel
+import json
+import hashlib
 
 class Object_json(BaseModel):
     id: int
     name: str
 
     def check_cache(self):
-        if self.id is 4:
+        if self.id == 4:
             return True
         else:
             return False
+
+    def jsonfy(self):
+        json_format = {
+            "id": self.id,
+            "name": self.name
+        }
+
+        dumped_json = json.dumps(json_format, sort_keys = True).encode("utf-8")
+        
+        return dumped_json
+
+    def get_body_hash(self):
+        dumped_json = self.jsonfy()
+        
+        hash_json = hashlib.sha1(dumped_json).hexdigest()
+
+        return hash_json
