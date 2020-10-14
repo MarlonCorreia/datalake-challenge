6c672962ed3dbea1b0efe46f56876e4d39d364f2
marloncorreia <malilocsimas26@gmail.com>
2020-10-14 04:55:50 -0300

    create post endpoint

--- part-1/src/main.py
+++ part-1/src/main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
+
+class Object_json(BaseModel):
+    id: int
+    name: str
+
+    def check_cache(self):
+        if self.id is 4:
+            return True
+        else:
+            return False
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Starting API"}
+@app.post("/post/")
+async def create_item(object: Object_json):
+    return_status = object.check_cache()
+
+    if return_status is True:
+        raise HTTPException(status_code=403, detail="Same body request")
+
+    return object
