d6d3a7ac63d4b40a8f3c0ff1f5e3489b965bbb5a
marloncorreia <malilocsimas26@gmail.com>
2020-10-15 01:29:30 -0300

    changing value property

--- part-1/src/cache.py
+++ part-1/src/cache.py
@@ -2,9 +2,8 @@ import redis
 
 redis = redis.Redis('172.17.0.2')
 
-def insert_cache(hash_json):
-    value = redis.dbsize() 
-    redis.set(hash_json, value + 1, ex=600)
+def insert_cache(hash_json):  
+    redis.set(hash_json, hash_json, ex=600)
 
 def check_if_in_cache(hash_json):
     if redis.exists(hash_json):
@@ -12,4 +11,3 @@ def check_if_in_cache(hash_json):
     else:
         insert_cache(hash_json)
         return False
-
