a4907a8d771de4a147f7dcc27382575946b81948
marloncorreia <malilocsimas26@gmail.com>
2020-10-15 01:27:37 -0300

    passing list of json in post request

--- part-1/src/main.py
+++ part-1/src/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI, status, HTTPException
+from typing import List
 from classes import Object_json
 
 app = FastAPI()
 
 
 @app.post("/post/")
-async def create_item(object: Object_json):
-    return_status = object.check_cache()
+async def create_item(object: List[Object_json]):
+    main_object = object[0]
+    return_status = main_object.check_cache()
 
     if return_status is True:
         raise HTTPException(status_code=403, detail="Same body request")
 
-    return object
+    return main_object
