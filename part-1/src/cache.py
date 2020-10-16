46c3e68993c63fc952ac03a4eb656254f1a6dc68
marloncorreia <malilocsimas26@gmail.com>
2020-10-16 00:21:37 -0300

    fake redis for testing

--- part-1/src/cache.py
+++ part-1/src/cache.py
@@ -1,4 +1,5 @@
 import redis
+import fakeredis
 
 redis = redis.Redis('172.17.0.2')
 
@@ -10,4 +11,17 @@ def check_if_in_cache(hash_json):
         return True
     else:
         insert_cache(hash_json)
+        return False
+
+"""Creating fake_redis for testing purposes"""
+
+fake_redis = fakeredis.FakeStrictRedis()
+
+def fake_redis_insert_cache(hash_json):
+    fake_redis.set(hash_json, hash_json, ex=600)
+
+def fake_redis_check_if_in_cache(hash_json):
+    if fake_redis.exists(hash_json):
+        return True
+    else:
         return False
