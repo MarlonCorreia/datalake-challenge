59c0e0f4c12f7a65418179d7f2f9586f688f88f2
marloncorreia <malilocsimas26@gmail.com>
2020-10-15 23:41:24 -0300

    accept any json body in post request

--- part-1/src/classes.py
+++ part-1/src/classes.py
@@ -1,12 +1,16 @@
-from pydantic import BaseModel
-from typing import List
 import json
 import hashlib
 from cache import check_if_in_cache
 
-class Object_json(BaseModel):
-    id: int
-    name: str
+class Object_json():
+
+    def __init__(self, json_body):
+        self.json_body = json_body
+
+    def dump_json(self):
+        dumped_json = json.dumps(self.json_body, sort_keys = True).encode("utf-8")
+        
+        return dumped_json
 
     def check_cache(self):
         body = self.get_body_hash()
@@ -14,18 +18,8 @@ class Object_json(BaseModel):
 
         return bool_status
 
-    def jsonfy(self):
-        json_format = {
-            "id": self.id,
-            "name": self.name
-        }
-
-        dumped_json = json.dumps(json_format, sort_keys = True).encode("utf-8")
-        
-        return dumped_json
-
     def get_body_hash(self):
-        dumped_json = self.jsonfy()
+        dumped_json = self.dump_json()
         
         hash_json = hashlib.sha1(dumped_json).hexdigest()
 
--- part-1/src/main.py
+++ part-1/src/main.py
@@ -1,16 +1,25 @@
-from fastapi import FastAPI, status, HTTPException
-from typing import List
+from fastapi import FastAPI, status, HTTPException, Request, Body
+from typing import List, Dict, Any
 from classes import Object_json
 
 app = FastAPI()
 
 
-@app.post("/post/")
-async def create_item(object: List[Object_json]):
-    main_object = object[0]
-    return_status = main_object.check_cache()
+#@app.post("/post/")
+#async def create_item(object: List[Object_json]):
+#    main_object = object[0]
+#    return_status = main_object.check_cache()
+#
+#    if return_status is True:
+#        raise HTTPException(status_code=403, detail="Same body request")
 
-    if return_status is True:
+#    return main_object
+
+@app.post("/post/")
+async def create_item(request = Body(...)):
+    object_json = Object_json(request)
+
+    if object_json.check_cache():
         raise HTTPException(status_code=403, detail="Same body request")
 
-    return main_object
+    return request
